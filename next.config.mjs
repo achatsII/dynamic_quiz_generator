edd12484b2b1aa8dbbae03f8d8d1d782597eb82d
Mouloud <achats@intelligenceindustrielle.com>
2025-11-13 15:22:37 -0500

    allow iframes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,24 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Allow embedding in iframes
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *", // Allow all origins to embed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
